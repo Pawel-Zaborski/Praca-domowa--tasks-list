3a526572113490054cdf57084694173df1925bd6
Pawel Zaborski <pawel.zaborski.uk@gmail.com>
2024-05-09 10:33:28 +0200

    task done

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
     for (const task of tasks) {
       htmlString += `
-          <li>
+          <li${tasks.done ? ' style="text-decoration: line through"' : ""}>
             ${task.content}
           </li>        
         `;
